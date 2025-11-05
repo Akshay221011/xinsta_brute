6a345be6def591d1500a58cd699135f88c156eff
Akshay221011 <ay7747369@gmail.com>
2025-11-05 21:12:53 +0530

    Remove Instagram command from script

--- 10M-pass/10M-pass.sh
+++ 10M-pass/10M-pass.sh
@@ -32,8 +32,6 @@ then
 echo -e  "$ylo >>> exiting........! Bye Bye :) <<<$rset"        
 else
 
-  instagram-py --username $usrnm --password-list $inspass
-
 echo
 sleep 30.0
 cd /data/data/com.termux/files/home
